5bcb85cee8c35f5a7589b6fb1adce7bdf52d38ed
g-dailey <gulafroz.rezai@gmail.com>
2023-01-01 09:53:39 -0800

    Solved Compressed Problem

--- uncompress.py
+++ uncompress.py
@@ -2,12 +2,14 @@
 
 uncompress:
 
-Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
+Write a function, uncompress, that takes in a string as an argument. The input string will be formatted
+into multiple groups according to the following pattern:
 
 <number><char>
 
 for example, '2c' or '3a'.
-The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively.
+You may assume that the input string is well-formed according to the previously mentioned pattern.
 
 test_00:
 uncompress("2c3a1t") # -> 'ccaaat'
