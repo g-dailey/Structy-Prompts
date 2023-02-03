dd1715b4a62a519a8070418c305706736d540ee2
g-dailey <gulafroz.rezai@gmail.com>
2023-02-03 11:15:22 -0800

    Solved Breadth First Value problem for Binary Tree

---
+++ BinaryTree/breadth_first_value.py
@@ -0,0 +1,54 @@
+'''
+breadth first values
+Write a function, breadth_first_values, that takes in the root of a binary tree. The function should return a list containing all values of the tree in breadth-first order.
+
+test_00:
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+breadth_first_values(a)
+#    -> ['a', 'b', 'c', 'd', 'e', 'f']
+
+'''
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def breadth_first_values(root):
+
+  if root == None:
+    return []
+  queue = [root]
+  answer = []
+
+  while len(queue) > 0:
+    first_item = queue[0]
+    if first_item.left:
+      queue.append(first_item.left)
+
+    if first_item.right:
+      queue.append(first_item.right)
+    answer.append(first_item.val)
+    queue = queue[1:]
+  print(answer)
+
+  return answer
