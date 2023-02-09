f14edaef8564a54e470aeb79029957d03d303d10
g-dailey <gulafroz.rezai@gmail.com>
2023-02-09 13:49:18 -0800

    Solved Add All Tree Path Prompt

--- BinaryTree/all_tree_path.py
+++ BinaryTree/all_tree_path.py
@@ -1,7 +1,8 @@
 
 '''
 all tree paths
-Write a function, all_tree_paths, that takes in the root of a binary tree. The function should return a 2-Dimensional list where each subarray represents a root-to-leaf path in the tree.
+Write a function, all_tree_paths, that takes in the root of a binary tree.
+The function should return a 2-Dimensional list where each subarray represents a root-to-leaf path in the tree.
 
 The order within an individual path must start at the root and end at the leaf, but the relative order among paths in the outer list does not matter.
 
@@ -47,22 +48,21 @@ def all_tree_paths(root):
   if root is None:
     return []
 
-  if root.left is None and root.right is None:
+  if root.right is None and root.left is None:
     return [[root.val]]
+  all_paths = []
+  left_path = all_tree_paths(root.left)
+  for path in left_path:
+    print(path)
+    all_paths.append([root.val, *path])
 
-  paths = []
-  left_sub_paths = all_tree_paths(root.left)
+  right_path = all_tree_paths(root.right)
+  for path in right_path:
+    print(path)
+    all_paths.append([root.val, *path])
+  return all_paths
 
-  for sub_path in left_sub_paths:
-    print(sub_path)
-    paths.append([root.val, *sub_path])
 
-  right_sub_paths = all_tree_paths(root.right)
-  for sub_path in right_sub_paths:
-    # print(sub_path)
-    paths.append([root.val, *sub_path])
-
-  return paths
 
 a = Node('a')
 b = Node('b')
