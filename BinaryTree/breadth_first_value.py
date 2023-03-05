b24bd63d1cc733631dffa6d4e14f64d3a2f1f4de
g-dailey <gulafroz.rezai@gmail.com>
2023-03-05 07:09:21 -0800

    Pracriced Basic Tree Problems for Second Round

--- BinaryTree/breadth_first_value.py
+++ BinaryTree/breadth_first_value.py
@@ -27,28 +27,131 @@ breadth_first_values(a)
 
 '''
 
+
+
+from collections import deque
+
 class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
 
-def breadth_first_values(root):
+def breadth_first_value(root):
+    if not root:
+        return []
 
-  if root == None:
-    return []
-  queue = [root]
-  answer = []
+    queue = deque([root])
+    values = []
 
-  while len(queue) > 0:
-    first_item = queue[0]
-    if first_item.left:
-      queue.append(first_item.left)
+    while queue:
+        node = queue.popleft()
+        values.append(node.val)
 
-    if first_item.right:
-      queue.append(first_item.right)
-    answer.append(first_item.val)
-    queue = queue[1:]
-  print(answer)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
 
-  return answer
+    return values
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+print(breadth_first_value(a))
+
+
+
+
+
+
+
+
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.right = None
+#         self.left = None
+
+# def breadth_first_value(root):
+#     if not root:
+#         return []
+
+#     queue = [root]
+#     values = []
+
+#     while queue:
+#         item = queue[0]
+#         values.append(item.val)
+
+#         if item.left:
+#             queue.append(item.left)
+#         if item.right:
+#             queue.append(item.right)
+#         queue = queue[1:]
+#     return values
+
+
+
+# a = Node('a')
+# b = Node('b')
+# c = Node('c')
+# d = Node('d')
+# e = Node('e')
+# f = Node('f')
+
+# a.left = b
+# a.right = c
+# b.left = d
+# b.right = e
+# c.right = f
+
+# print(breadth_first_value(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # class Node:
+# #   def __init__(self, val):
+# #     self.val = val
+# #     self.left = None
+# #     self.right = None
+
+# # def breadth_first_values(root):
+
+# #   if root == None:
+#   #   return []
+#   # queue = [root]
+#   # answer = []
+
+#   # while len(queue) > 0:
+#   #   first_item = queue[0]
+#   #   if first_item.left:
+#   #     queue.append(first_item.left)
+
+#   #   if first_item.right:
+#   # #     queue.append(first_item.right)
+#   # #   answer.append(first_item.val)
+#   #   queue = queue[1:]
+#   # print(answer)
+
+#   # return answer
--- BinaryTree/dept_first_value.py
+++ BinaryTree/dept_first_value.py
@@ -1,14 +1,9 @@
 '''
+Write a function, depth_first_values, that takes in the root of a binary tree.
+The function should return a list containing all values of the tree in depth-first order.
 
-
-structy-logo
-structy
-premium
-favorite
-depth first values
-Write a function, depth_first_values, that takes in the root of a binary tree. The function should return a list containing all values of the tree in depth-first order.
-
-Hey. This is our first binary tree problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
+Hey. This is our first binary tree problem, so you should be liberal with watching the Approach and Walkthrough.
+Be productive, not stubborn. -AZ
 
 test_00:
 a = Node('a')
@@ -33,23 +28,74 @@ depth_first_values(a)
 #   -> ['a', 'b', 'd', 'e', 'c', 'f']
 
 '''
-
 class Node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
 
-def depth_first_values(root):
-  if root == None:
-    return []
-  answer = [root.val]
-  left = []
-  right = []
-  if root.right:
-    right = depth_first_values(root.right)
 
-  if root.left:
-    left =  depth_first_values(root.left)
+def depth_first_value(root):
 
-  return answer + left + right
+    if not root:
+        return []
+
+    stack = [root]
+    values = []
+
+    while stack:
+        item = stack.pop()
+        values.append(item.val)
+
+        if item.right:
+            stack.append(item.right)
+        if item.left:
+            stack.append(item.left)
+    return values
+
+
+
+
+
+
+
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+print(depth_first_value(a))
+
+
+
+
+
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+# def depth_first_values(root):
+#   if root == None:
+#     return []
+#   answer = [root.val]
+#   left = []
+#   right = []
+#   if root.right:
+#     right = depth_first_values(root.right)
+
+#   if root.left:
+#     left =  depth_first_values(root.left)
+
+  # return answer + left + right
